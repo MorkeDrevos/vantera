7b84ebf23a7615fe4c53674774634ef7836c034f
Mørke Drevos <drtseo@proton.me>
2026-01-09 10:43:05 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'plus.unsplash.com' },
+      { protocol: 'https', hostname: 'source.unsplash.com' },
     ],
   },
 };
