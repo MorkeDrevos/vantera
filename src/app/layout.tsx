ce2c4496fb91c18e2e4588bf5bdcfda8b19989ab
Mørke Drevos <drtseo@proton.me>
2026-01-09 01:58:46 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
+import type { Metadata } from 'next';
 import './globals.css';
-import type { ReactNode } from 'react';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Locus',
   description: 'Find cities, beautifully.',
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
