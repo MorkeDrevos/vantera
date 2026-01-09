6e31f3742d80d5769c655493697873b42fe32475
Mørke Drevos <drtseo@proton.me>
2026-01-09 11:01:47 +0100

    Create FeaturedRoutesClient.tsx

---
+++ src/components/home/FeaturedRoutesClient.tsx
@@ -0,0 +1,14 @@
+// src/components/home/PopularGridClient.tsx
+'use client';
+
+import type { City } from './cities';
+import CityCardsClient from './CityCardsClient';
+
+export default function PopularGridClient({ cities }: { cities: City[] }) {
+  return (
+    <CityCardsClient
+      cities={cities}
+      columns="grid-cols-1 sm:grid-cols-2 lg:grid-cols-2"
+    />
+  );
+}
