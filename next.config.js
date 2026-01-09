59ea113f3e6e8681cad4de460780fb7a196ad052
Mørke Drevos <drtseo@proton.me>
2026-01-09 08:25:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'unsplash.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
