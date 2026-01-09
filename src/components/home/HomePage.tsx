86157159fd634df4849d8e357315e4d7fa9ee06c
Mørke Drevos <drtseo@proton.me>
2026-01-09 15:58:29 +0100

    Update HomePage.tsx

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -74,9 +74,10 @@ function SectionLabel({ children }: { children: React.ReactNode }) {
   );
 }
 
-function findCities(slugs: string[]) {
-  const map = new Map(CITIES.map((c) => [c.slug, c]));
-  return slugs.map((s) => map.get(s)).filter(Boolean) as typeof CITIES;
+function findCities(slugs: readonly string[]) {
+  type City = (typeof CITIES)[number];
+  const map = new Map<string, City>(CITIES.map((c) => [c.slug, c]));
+  return slugs.map((s) => map.get(s)).filter(Boolean) as City[];
 }
 
 export default function HomePage() {
