26a4814aa187925e3cf55d1aab7385a9581e9311
Mørke Drevos <drtseo@proton.me>
2026-01-12 00:27:09 +0100

    Create SellerUploadClient.tsx

---
+++ src/components/sell/SellerUploadClient.tsx
@@ -0,0 +1,10 @@
+// src/components/sell/SellerUploadClient.tsx
+'use client';
+
+export default function SellerUploadClient() {
+  return (
+    <div className="rounded-2xl border border-white/10 bg-white/[0.03] p-6 text-zinc-300">
+      Seller upload flow coming soon.
+    </div>
+  );
+}
