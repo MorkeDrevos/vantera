87766df471eecb6239c26e2c24df687713c28da5
Mørke Drevos <drtseo@proton.me>
2026-01-11 01:42:15 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'source.unsplash.com' },
       { protocol: 'https', hostname: 'plus.unsplash.com' },
+
+      // Brandfetch CDN (logos)
+      { protocol: 'https', hostname: 'cdn.brandfetch.io' },
     ],
   },
 };
