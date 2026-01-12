cf4468ff9756fa04de55881eddcd374c70b5f8a0
Mørke Drevos <drtseo@proton.me>
2026-01-12 07:32:21 +0100

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,7 +8,9 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    log: ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
