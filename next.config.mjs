ba96fec8b1e3029ea6321f2ec9a76f228f99b97a
Mørke Drevos <drtseo@proton.me>
2026-01-15 20:12:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'source.unsplash.com' },
-      { protocol: 'https', hostname: 'plus.unsplash.com' },
-
-      // Brandfetch CDN (logos)
-      { protocol: 'https', hostname: 'cdn.brandfetch.io' },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
