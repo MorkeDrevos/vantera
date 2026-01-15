670aac959c6cd7222fa6066ad9a5f0b7d1588b4d
Mørke Drevos <drtseo@proton.me>
2026-01-15 21:47:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
